30b8f8707e28faa68f46c3ad625d47b7c2b2631d
Artjom Lell <artjom.lell@web.de>
2019-04-07 12:58:38 +0200

    added dummy images for play items

--- app/src/main/java/org/artjom/tictacttoe/extended/MainActivity.java
+++ app/src/main/java/org/artjom/tictacttoe/extended/MainActivity.java
@@ -1,13 +1,23 @@
 package org.artjom.tictacttoe.extended;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        ImageView image1 = findViewById(R.id.playItem1);
+
+        image1.setVisibility(View.INVISIBLE);
+    }
+
+    public void click(View view) {
     }
 }
